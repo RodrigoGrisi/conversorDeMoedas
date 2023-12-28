707590889069b0265f80d072af4a9f6e089dcd71
Rodrigo Grisi <46337889+RodrigoGrisi@users.noreply.github.com>
2023-12-28 18:21:42 -0300

    Starting the project

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.areaMoeda}>
+        <Text style={styles.Titulo}> Seleciona sua moeda</Text>
+      </View>
+      <Text style={{ color: '#fff' }}> Testando</Text>
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
@@ -13,8 +19,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#101215',
     justifyContent: 'center',
+    padding: 40,
+    alignItems: 'center'
   },
+  areaMoeda:{
+    backgroundColor: '#f9f9f9',
+    width: '90%',
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+    padding:10,
+  },Titulo:{
+    color: '#000',
+    textAlign: 'center'
+  }
 });
